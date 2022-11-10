e01e6d2a4a0a02b2223dc3c536a893091e2d102e
RuoningJin <ronjin2013@hotmail.com>
2022-11-09 20:53:35 -0500

    escape function added to client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,67 +5,89 @@
  */
 
 $(document).ready(function() {
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
+  //rendering function to append user data onto the webpage
   const renderTweets = function(tweets) {
-    
-    for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      console.log($tweet);
-      $('#tweet-container').append($tweet);
+    if (Array.isArray(tweets)) {
+      for (const tweet of tweets) {
+        const $tweet = createTweetElement(tweet);
+        $('#tweet-container').prepend($tweet);
+      }
     }
-  }
+    if (!Array.isArray(tweets)) {
+      $('#tweet-container').prepend(createTweetElement(tweets));
+    }
+  };
+  //escape function
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
+  // new html markup adding function
   const createTweetElement = function(data) {
     const userInfo = data.user;
     const markup = `
       <article class="tweet">
-                <header class="space-between">               
-                  <div>
-                    <img src="${userInfo.avatars}" alt="user-avatar">
-                    &nbsp;${userInfo.name}
-                  </div>
-                  <span>
-                    ${userInfo.handle}
-                  </span>
-                </header>
-                <p>${data.content.text}</p>
-                <footer class="space-between">
-                  <div>
-                    ${data.created_at}
-                  </div>
-                  <i>
-                    <i class="fa-sharp fa-solid fa-flag"></i>
-                    <i class="fa-solid fa-retweet"></i>
-                    <i class="fa-solid fa-heart"></i>
-                  </i>
-                </footer>
-              </article>
-      `;
+        <header class="space-between">               
+          <div>
+            <img src="${(userInfo.avatars)}" alt="user-avatar">
+            &nbsp;${userInfo.name}
+          </div>
+          <span>
+            ${userInfo.handle}
+          </span>
+        </header>
+          <p>${escape(data.content.text)}</p>
+        <footer class="space-between">
+          <div>
+            ${timeago.format(data.created_at)}
+          </div>
+          <i>
+            <i class="fa-sharp fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </i>
+        </footer>
+      </article>
+      <br>
+    `;
     return markup;
   };
-  renderTweets(tweetData);
+
+  const loadtweets = function() {
+    $.get({
+      url:'/tweets',
+    })
+      .then(function(userData) {
+        renderTweets(userData);
+      });
+  };
+
+  loadtweets();
+
+  $('form').submit(function(event) {
+    event.preventDefault();
+    const dataSer = $(this).serialize();
+
+    if (dataSer === 'text=') {
+      return alert('The content cannot be blank.');
+    }
+    if (dataSer.length >= 145) {
+      return alert('The content exceeded the limit of 140 characters.');
+    }
+    $.post({
+      url: '/tweets',
+      data: dataSer,
+    })
+      .then(() => {
+        $.get({
+          url:'/tweets',
+        })
+          .then(function(userData) {
+            renderTweets(userData[userData.length - 1]);
+          });
+      });
+  });
 });
